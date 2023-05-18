0459b5035df455cb706ca2fe4cc60b174c3f2c01
Edumassami <du.massami@outlook.com>
2023-05-18 20:22:21 -0300

    like / deslike

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -3,11 +3,21 @@ import { RespostaPadraoMsg } from "../../types/RespostaPadraoMsg";
 import { conectarMongoDB } from "../../middlewares/conectaMongoDB";
 import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
 import { UsuarioModel } from "@/models/UsuarioModel";
+import usuario from "./usuario";
 
 const pesquisaEndpoint = 
     async(req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any[]>) => {
         try{
             if(req.method === 'GET'){
+
+                if(req?.query?.id){
+                    const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id);
+                    if(!usuarioEncontrado){
+                        return res.status(400).json({erro : 'Usuário não encontrado'});
+                    }
+                    usuarioEncontrado.senha = null;
+                    return res.status(200).json(usuarioEncontrado); 
+                }else{
 
                 const {filtro} = req.query;
 
@@ -23,6 +33,7 @@ const pesquisaEndpoint =
                 return res.status(200).json(usuariosEncontrados);
             }
             return res.status(400).json({erro : 'Método informado não é válido'});
+        }
         }catch(e){
             console.log(e);
             return res.status(400).json({erro : 'Não foi possível buscar usuário'});
